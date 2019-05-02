077a01f2ab77539b43d4a206a55fcb5961dd8624
digo_ <digo_@LAPTOP-IJRTRRO8>
2019-05-02 14:35:28 -0300

    new changes

---
+++ tdb-monitor/tdb-monitor-web/src/main/java/br/com/toyota/tdbmonitor/web/beans/DashboardBean.java
@@ -0,0 +1,10 @@
+package br.com.toyota.tdbmonitor.web.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean(name = "dashboardBean")
+public class DashboardBean {
+
+}
